6f57bcd980903cebfb87db4d26d1d63f77acf919
duddke <sya1008@naver.com>
2022-10-18 15:49:44 +0900

    포톤 및 조이스틱 작업

--- Assets/SYA/Scripts/SYA_InputManager.cs
+++ Assets/SYA/Scripts/SYA_InputManager.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class SYA_InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static bool bDown = false;
+    static Vector3 firstPosition;
+
+    public static float GetAxis(string axis)
     {
-        
+        //안드로이드라면
+#if Androids
+        bool bJoystick = true;
+        //PC라면
+#elif PC
+        bool bJoystick = false;
+#endif
+        if (bJoystick == false)
+        {
+            return Input.GetAxis(axis);
+        }
+        //안드로이드라면
+        if (axis == "Horizontal")
+        {
+            return GetDirection().x;
+        }
+        else if (axis == "Vertical")
+        {
+            return GetDirection().y;
+        }
+
+        return 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    // 방향구하기
+    public static Vector3 GetDirection()
     {
-        
+        Vector3 dir = Vector3.zero;
+        Touch touch = Input.GetTouch(0);
+        // 사용자가 처음 클릭했다면
+        if (touch.phase == TouchPhase.Began)
+        {
+            // 클릭 중이다.
+            bDown = true;
+            // 첫번째 점은 클릭한 지점의 마우스 위치
+            firstPosition = touch.position;
+        }
+        if (touch.phase == TouchPhase.Ended)
+        {
+            bDown = false;
+        }
+        // 마우스가 클릭중이라면
+        if (bDown)
+        {
+            //벡터의 뺄셈 -> 방향구하기
+            dir = new Vector3(touch.position.x, touch.position.y, 0) - new Vector3(firstPosition.x, firstPosition.y, 0);
+        }
+
+        return dir;//.normalized;
     }
 }
--- Assets/SYA/Scripts/SYA_PlayerMove.cs
+++ Assets/SYA/Scripts/SYA_PlayerMove.cs
@@ -3,46 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace SYA_PlayerMoveGroup
+public class SYA_PlayerMove : MonoBehaviour
 {
-#if PC
-    public class SYA_PlayerMove : MonoBehaviour
+    public float speed = 10;
+    public float jumpPower = 3;
+    public float jumpCount = 0;
+    public float yVelocity;
+    public float gravity = -9.81f;
+    public CharacterController cc;
+    public Text nickName;
+    // Start is called before the first frame update
+    void Start()
     {
-        public float speed = 10;
-        public float jumpPower = 3;
-        public float jumpCount = 0;
-        public float yVelocity;
-        public float gravity = -9.81f;
-        public CharacterController cc;
-        public Text nickName;
-        // Start is called before the first frame update
-        void Start()
-        {
-            cc = GetComponent<CharacterController>();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
-            Vector3 dir = Vector3.forward * v + Vector3.right * h;
-            dir = Camera.main.transform.TransformDirection(dir);
-            dir.Normalize();
-            if (cc.isGrounded)
-            {
-                yVelocity = 0;
-                jumpCount = 0;
-            }
-            if (Input.GetButtonDown("Jump") && jumpCount == 0)
-            {
-                jumpCount++;
-                yVelocity = jumpPower;
-            }
-            yVelocity += gravity * Time.deltaTime;
-            dir.y = yVelocity;
-            cc.Move(dir * speed * Time.deltaTime);
-        }
+        cc = GetComponent<CharacterController>();
     }
-#elif 
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = SYA_InputManager.GetAxis("Horizontal");
+        float v = SYA_InputManager.GetAxis("Vertical");
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        dir = Camera.main.transform.TransformDirection(dir);
+        dir.Normalize();
+        if (cc.isGrounded)
+        {
+            yVelocity = 0;
+            jumpCount = 0;
+        }
+        if (Input.GetButtonDown("Jump") && jumpCount == 0)
+        {
+            jumpCount++;
+            yVelocity = jumpPower;
+        }
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+        cc.Move(dir * speed * Time.deltaTime);
+    }
 }
+
